d6ea227ac475688ae9039145ecae8ffabc73d071
abena07 <62352130+abena07@users.noreply.github.com>
2021-03-04 20:52:16 +0000

    second commit

--- resources/views/blog-details.blade.php
+++ resources/views/blog-details.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Blog Detail ccc</div>
+                <div class="card-header">Blog Detail </div>
 
                 <div class="card-body">
                     <h2>{{ $blog->post_title }}</h2>
